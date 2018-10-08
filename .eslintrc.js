24971479546da5aa114435cd4165652bd9b96267
Azmi Makarima <amy.azmim@gmail.com>
2018-10-09 00:45:02 +0700

    change no-unused-vars to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
   env: {
-    browser: true
+    browser: true,
   },
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'warn',
-
     'no-await-in-loop': 'off',
+    'no-unused-vars': 'warn',
 
     // Rules below are all added to remove conflicts with prettier. DO NOT REMOVE
     'arrow-parens': 'off',
