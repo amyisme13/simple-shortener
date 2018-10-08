e2bd149a4c803085aa9003c1543abac3bcae2d61
Azmi Makarima <amy.azmim@gmail.com>
2018-10-09 00:59:46 +0700

    add store

--- public/js/db.js
+++ public/js/db.js
@@ -1,5 +1,12 @@
+/**
+ * Configs
+ */
 const TOKEN =
   'e62e33ae36a4c22a538eda34adfedcbd57d767e07cb2a47636ad2cf2d2ba3004';
+
+/**
+ * Helps
+ */
 const JSON_STORE_ENDPOINT = `https://www.jsonstore.io/${TOKEN}/urls`;
 
 class HTTPError extends Error {
@@ -17,6 +24,10 @@ class NotFoundError extends Error {
     this.id = id;
   }
 }
+
+/**
+ * Find
+ */
 
 const findByIdLocal = id => {
   const urlsString = localStorage.getItem('urls');
@@ -60,4 +71,45 @@ const findById = async id => {
   }
 
   return url;
+};
+
+/**
+ * Store
+ */
+
+const storeLocal = async url => {
+  const urlsString = localStorage.getItem('urls');
+
+  let urls;
+  if (!urlsString) {
+    urls = {};
+  } else {
+    urls = JSON.parse(urlsString);
+  }
+  if (!urls[url.id]) {
+    urls[url.id] = url;
+  }
+
+  localStorage.setItem('urls', JSON.stringify(urls));
+};
+
+const storeJsonStore = async url => {
+  await fetch(`${JSON_STORE_ENDPOINT}/${url.id}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      original: url.original,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const store = async url => {
+  if (!url.id || !url.original) {
+    throw new Error('Invalid URL Object');
+  }
+
+  await storeLocal(url);
+  await storeJsonStore(url);
 };
